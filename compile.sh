fa2e1fc894cd52ecd4e49e8db856d4bf54b89b79
fabio-terranova <fabio.terranova@outlook.it>
2025-12-18 18:05:21 +0100

    fix: compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,1 @@
-g++ -std=c++20 -Wall -o build/server src/main.cpp src/hw_peripherals.cpp src/acquirer.cpp s
-rc/server.cpp -lbcm2835 -lpthread -lrt
+g++ -std=c++20 -Wall -o build/server src/main.cpp src/hw_peripherals.cpp src/acquirer.cpp src/server.cpp -lbcm2835 -lpthread -lrt
